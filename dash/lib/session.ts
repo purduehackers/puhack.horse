20838a7a5d85b902aa5f84b4ef58b4af222d6f51
Matthew Stanciu <mattbstanciu@gmail.com>
2023-01-21 23:42:49 -0500

    frontend –> dash

--- dash/lib/session.ts
+++ dash/lib/session.ts
@@ -1,6 +1,6 @@
 import { unstable_getServerSession } from "next-auth/next";
 
-import { authOptions } from "../lib/auth";
+import { authOptions } from "./auth";
 
 export async function getSession() {
   return await unstable_getServerSession(authOptions);
