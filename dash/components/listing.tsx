77050f7e38eb5c1f47487463226539b8b4f6ba20
Matthew Stanciu <mattbstanciu@gmail.com>
2023-01-24 14:15:11 -0500

    Use lucide icons

--- dash/components/listing.tsx
+++ dash/components/listing.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { CheckSquare, Edit, XSquare } from "react-feather";
+import { CheckSquare, Edit, XSquare } from "lucide-react";
 import useSWR from "swr";
 import { KVData } from "../types/types";
 
