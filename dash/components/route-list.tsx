7b3ec44195059d44fde6ffbec647b2a6141d5058
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-06 18:08:41 -0500

    bottom margin

--- dash/components/route-list.tsx
+++ dash/components/route-list.tsx
@@ -42,7 +42,7 @@ const RouteList = ({ fallback }: { fallback: KVData[] }) => {
               </p>
             </div>
           </div>
-          <div className="rounded-sm border-[3px] border-black w-11/12 sm:max-w-xl tabular-nums shadow-container shadow-gray-900/70 overflow-y-auto">
+          <div className="rounded-sm border-[3px] border-black w-11/12 sm:max-w-xl tabular-nums shadow-container shadow-gray-900/70 overflow-y-auto mb-8">
             <div className="py-1 flex flex-row bg-gray-200 border-b-[3px] border-b-black">
               <p className="font-bold text-center w-5/12 sm:w-1/3">route</p>
               <p className="flex-1 font-bold text-center pl-2">destination</p>
