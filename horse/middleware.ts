842b43b456c76bac92d52faa78193902048d9c45
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-07 14:40:33 -0500

    increment visits

--- horse/middleware.ts
+++ horse/middleware.ts
@@ -5,8 +5,24 @@ import { get } from "@vercel/edge-config";
 export async function middleware(req: NextRequest) {
   try {
     const route = req.nextUrl.pathname.slice(1);
-    const destination = await get(route);
-    return NextResponse.redirect(destination.destination);
+    const data = await get(route);
+    fetch("http://localhost:3000/api/dash", {
+      method: "PATCH",
+      body: JSON.stringify({
+        items: [
+          {
+            operation: "update",
+            key: route,
+            value: {
+              route,
+              destination: data.destination,
+              visits: data.visits + 1,
+            },
+          },
+        ],
+      }),
+    });
+    return NextResponse.redirect(data.destination);
   } catch (err) {
     return NextResponse.redirect("https://purduehackers.com");
   }
