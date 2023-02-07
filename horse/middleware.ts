9c6ed5900f6b9a6ac64f74c38e9195234574b9d4
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-07 03:08:38 -0500

    Make it work

--- horse/middleware.ts
+++ horse/middleware.ts
@@ -4,6 +4,9 @@ import { get } from "@vercel/edge-config";
 
 export async function middleware(req: NextRequest) {
   const route = req.nextUrl.pathname.slice(1);
+  if (!route) {
+    return NextResponse.redirect("https://purduehackers.com");
+  }
   const destination = await get(route);
   if (!destination) {
     return NextResponse.redirect("https://purduehackers.com");
