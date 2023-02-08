88087066b28feae9608251cf3868f89d27a4cd4c
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-07 22:09:47 -0500

    More homepage

--- dash/app/page.tsx
+++ dash/app/page.tsx
@@ -8,15 +8,20 @@ async function Index() {
     return redirect("/dashboard");
   }
   return (
-    <div className="flex h-screen flex-col items-center justify-center py-2">
-      <main className="flex w-full flex-1 flex-col gap-4 items-center justify-center px-20 text-center">
-        <h1 className="text-center text-amber-600 font-mono text-4xl font-bold">
+    <div className="flex h-screen flex-col items-center justify-center py-2 font-main">
+      <main className="flex w-full flex-1 flex-col gap-4 items-center justify-center px-4 md:px-20">
+        <h1 className="text-amber-600 font-mono text-3xl sm:text-6xl font-bold">
           <span className="select-none inline-block shake">🐴</span>{" "}
           puhack.horse{" "}
           <span className="select-none inline-block shake">🐴</span>
         </h1>
-        <div className="border-2 border-black bg-gray-200 p-2 rounded">
-          <p>Edge Config-based link shortener</p>
+        <div className="border-2 border-black bg-gray-200 p-2 rounded max-w-lg flex flex-col gap-2">
+          <p>Hello!!!! You've found the admin dashboard for puhack.horse</p>
+          <p>
+            You can only access this if you're on the Core team in the Purdue
+            hackers GitHub org. If you think you should be in there, but can't
+            access the dashboard, ping @matthew!!!
+          </p>
         </div>
         <SignInButton />
       </main>
