a19f57911b549a263ffeb7349bf5eb4b0e5a9305
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-07 22:05:57 -0500

    small homepage thing

--- dash/app/page.tsx
+++ dash/app/page.tsx
@@ -10,7 +10,11 @@ async function Index() {
   return (
     <div className="flex h-screen flex-col items-center justify-center py-2">
       <main className="flex w-full flex-1 flex-col gap-4 items-center justify-center px-20 text-center">
-        <h1 className="text-4xl font-bold text-center">puhack.horse 🐴</h1>
+        <h1 className="text-center text-amber-600 font-mono text-4xl font-bold">
+          <span className="select-none inline-block shake">🐴</span>{" "}
+          puhack.horse{" "}
+          <span className="select-none inline-block shake">🐴</span>
+        </h1>
         <div className="border-2 border-black bg-gray-200 p-2 rounded">
           <p>Edge Config-based link shortener</p>
         </div>
--- dash/components/sign-in-button.tsx
+++ dash/components/sign-in-button.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 export default function SignInButton() {
   return (
     <button
-      className="p-2 text-lg font-bold bg-blue-300 hover:bg-blue-400 border-2 border-black rounded-lg transition duration-100"
+      className="p-2 text-lg font-bold bg-amber-500 hover:bg-amber-400 border-2 border-black rounded-lg transition duration-100"
       onClick={() => signIn("github")}
     >
       Sign In with GitHub
