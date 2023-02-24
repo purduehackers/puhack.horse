4ab9416051ad15aad08c4699c070482591f95a88
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-24 16:31:49 -0500

    Add title to erase

--- dash/components/erase.tsx
+++ dash/components/erase.tsx
@@ -26,6 +26,9 @@ const Erase = ({
       <AlertDialog.Portal>
         <AlertDialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
         <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <AlertDialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+            Delete route
+          </AlertDialog.Title>
           <AlertDialog.Description className="text-mauve11 mt-4 mb-5 text-[15px] leading-normal">
             Are you sure you want to delete this route?
           </AlertDialog.Description>
