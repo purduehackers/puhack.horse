06fdbc2b0b514f96461226ff4e15da7f75a7835f
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-24 17:15:51 -0500

    Add authentication to API i think

--- dash/pages/api/dash.ts
+++ dash/pages/api/dash.ts
@@ -2,14 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { getAll } from "@vercel/edge-config";
 
 export default async (req: NextRequest) => {
-  // const authorization = req.headers.get("Authorization");
-  // if (authorization !== `Bearer ${process.env.HORSE_SECRET}`) {
-  //   console.log("huh????", authorization);
-  //   return new Response(null, {
-  //     status: 403,
-  //     statusText: `You need an API key to access this!!!!`,
-  //   });
-  // }
+  const authorization = req.headers.get("Authorization");
+  if (!authorization) {
+    return new Response(null, {
+      status: 400,
+      statusText: `You need an API key to access this!!!!`,
+    });
+  } else if (authorization !== `Bearer ${process.env.HORSE_SECRET}`) {
+    return new Response(null, {
+      status: 403,
+      statusText: `Incorrect API key!!!!!!!!!!!`,
+    });
+  }
   if (req.method === "GET") {
     const configItems = await getAll();
     return NextResponse.json(Object.values(configItems));
