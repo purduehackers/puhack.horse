ba256ef7f7da7ceaf9ab4610dad110afec290a98
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-25 04:02:11 -0500

    Add clarifying comment

--- horse/pages/index.tsx
+++ horse/pages/index.tsx
@@ -1,2 +1,6 @@
+// Next.js won't build this project unless I include at least one page.
+// This page will never be seen; `middleware.ts` will always redirect
+// the user to either the intended destination or purduehackers.com
+
 const Page = () => <h1>hi</h1>;
 export default Page;
