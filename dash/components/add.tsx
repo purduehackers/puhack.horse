808c8c54fc44b5d059d3e9dfb1f53d1b7931aa30
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-25 01:03:40 -0500

    update add dialog styling

--- dash/components/add.tsx
+++ dash/components/add.tsx
@@ -48,8 +48,8 @@ const Add = ({ fallbackData }: { fallbackData: ConfigData[] }) => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-sm bg-white border-[3px] border-black p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-bold">
             Add route
           </Dialog.Title>
           <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
@@ -58,13 +58,13 @@ const Add = ({ fallbackData }: { fallbackData: ConfigData[] }) => {
           </Dialog.Description>
           <fieldset className="mb-[15px] flex items-center gap-5">
             <label
-              className="text-violet11 w-[90px] text-right text-[15px]"
+              className="text-gray-800 font-bold w-[90px] text-right text-[15px]"
               htmlFor="route"
             >
               Route
             </label>
             <input
-              className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+              className="text-gray-800 shadow-amber-500 focus:shadow-amber-600 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
               autoComplete="off"
               autoCorrect="off"
               autoCapitalize="off"
@@ -76,13 +76,13 @@ const Add = ({ fallbackData }: { fallbackData: ConfigData[] }) => {
           </fieldset>
           <fieldset className="mb-[15px] flex items-center gap-5">
             <label
-              className="text-violet11 w-[90px] text-right text-[15px]"
+              className="text-gray-800 font-bold w-[90px] text-right text-[15px]"
               htmlFor="destination"
             >
               Destination
             </label>
             <input
-              className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+              className="text-gray-800 shadow-amber-500 focus:shadow-amber-600 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
               autoComplete="off"
               autoCorrect="off"
               autoCapitalize="off"
@@ -105,7 +105,7 @@ const Add = ({ fallbackData }: { fallbackData: ConfigData[] }) => {
           </div>
           <Dialog.Close asChild>
             <button
-              className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+              className="text-amber-600 hover:bg-amber-100 focus:shadow-amber-500 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
               aria-label="Close"
             >
               <X />
