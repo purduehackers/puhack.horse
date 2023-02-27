73cfb4b3ddd50c044717242ec6d417630dcb622d
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-27 16:04:13 -0500

    Revert "Verify puhack.horse links on Mastodon"
    This reverts commit 2c2a8b68ced819c9120e280e3c52674ad3fce747.

--- horse/pages/index.tsx
+++ horse/pages/index.tsx
@@ -2,7 +2,5 @@
 // This page will never be seen; `middleware.ts` will always redirect
 // the user to either the intended destination or purduehackers.com
 
-const Page = () => (
-  <a href="https://mastodon.boiler.social/@purduehackers" rel="me"></a>
-);
+const Page = () => <h1>hi</h1>;
 export default Page;
