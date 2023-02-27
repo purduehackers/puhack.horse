2c2a8b68ced819c9120e280e3c52674ad3fce747
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-27 16:01:42 -0500

    Verify puhack.horse links on Mastodon

--- horse/pages/index.tsx
+++ horse/pages/index.tsx
@@ -2,5 +2,7 @@
 // This page will never be seen; `middleware.ts` will always redirect
 // the user to either the intended destination or purduehackers.com
 
-const Page = () => <h1>hi</h1>;
+const Page = () => (
+  <a href="https://mastodon.boiler.social/@purduehackers" rel="me"></a>
+);
 export default Page;
