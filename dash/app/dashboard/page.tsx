67ada3c626db031f7a1655ee4d3f2b2fb6f039cb
Matthew Stanciu <mattbstanciu@gmail.com>
2023-03-02 00:08:18 -0500

    Fetch info without blocking rendering

--- dash/app/dashboard/page.tsx
+++ dash/app/dashboard/page.tsx
@@ -1,19 +1,58 @@
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import EmojiMarquee from "../../components/emoji-marquee";
 import RouteList from "../../components/route-list";
-import { server } from "../../lib/helpers";
+import UserInfo from "../../components/user-info";
 import { getCurrentUser } from "../../lib/session";
 
 export default async function DashboardPage() {
   const user = await getCurrentUser();
-  const data = await fetch(`${server}/api/dash`, {
-    headers: {
-      Authorization: `Bearer ${process.env.HORSE_SECRET}`,
-    },
-  }).then((r) => r.json());
 
   if (!user) {
     redirect("/");
   }
 
-  return <RouteList fallbackData={data} user={user} />;
+  return (
+    <div className="flex flex-col">
+      <UserInfo user={user} />
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center lg:items-start justify-center lg:flex-row w-full lg:w-11/12 xl:w-full gap-x-8">
+          <div className="flex flex-col gap-4 mx-4 max-w-sm">
+            <h1 className="text-center text-amber-600 font-mono text-3xl sm:text-4xl font-bold">
+              <span className="select-none inline-block shake">🐴</span>{" "}
+              puhack.horse{" "}
+              <span className="select-none inline-block shake">🐴</span>
+            </h1>
+            <hr className="h-1 border-0 bg-gray-600 rounded-sm" />
+            <div className="flex flex-col gap-2">
+              <p>
+                this is the dashboard for purdue hackers' link shortener.
+                welcome!!!
+              </p>
+              <p>
+                to edit something, just click on it in the table. your changes
+                will reflect on the dashboard immediately and update in edge
+                config in the background. it will take a few seconds to
+                propagate, but shouldn't take longer than that.
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <EmojiMarquee large={true} />
+            </div>
+          </div>
+          <EmojiMarquee />
+          <div className="rounded-sm border-[3px] border-black w-11/12 sm:max-w-2xl tabular-nums shadow-container shadow-gray-900/70 overflow-y-auto mb-8 mt-4 lg:mt-0">
+            <div className="py-1 flex flex-row bg-amber-500 border-b-[3px] border-b-black">
+              <p className="font-bold text-center w-5/12 sm:w-1/4">route</p>
+              <p className="flex-1 font-bold text-center pl-2">destination</p>
+              <p className="sm:w-20"></p>
+            </div>
+            <Suspense>
+              <RouteList />
+            </Suspense>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- dash/components/route-list.tsx
+++ dash/components/route-list.tsx
@@ -8,13 +8,14 @@ import EmojiMarquee from "./emoji-marquee";
 import Listing from "./listing";
 import UserInfo from "./user-info";
 
-const RouteList = ({
-  fallbackData,
-  user,
-}: {
-  fallbackData: ConfigData[];
-  user: User;
-}) => {
+const RouteList = () => {
+  const fallbackData = [
+    {
+      route: "loading...",
+      destination: "loading...",
+      visits: 0,
+    },
+  ];
   const { data } = useSWR(`${server}/api/dash`, fetcher, {
     suspense: true,
     fallbackData,
@@ -39,62 +40,26 @@ const RouteList = ({
   });
 
   return (
-    <div className="flex flex-col">
-      <UserInfo user={user} />
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col items-center lg:items-start justify-center lg:flex-row w-full lg:w-11/12 xl:w-full gap-x-8">
-          <div className="flex flex-col gap-4 mx-4 max-w-sm">
-            <h1 className="text-center text-amber-600 font-mono text-3xl sm:text-4xl font-bold">
-              <span className="select-none inline-block shake">🐴</span>{" "}
-              puhack.horse{" "}
-              <span className="select-none inline-block shake">🐴</span>
-            </h1>
-            <hr className="h-1 border-0 bg-gray-600 rounded-sm" />
-            <div className="flex flex-col gap-2">
-              <p>
-                this is the dashboard for purdue hackers' link shortener.
-                welcome!!!
-              </p>
-              <p>
-                to edit something, just click on it in the table. your changes
-                will reflect on the dashboard immediately and update in edge
-                config in the background. it will take a few seconds to
-                propagate, but shouldn't take longer than that.
-              </p>
-            </div>
-            <div className="flex justify-center">
-              <EmojiMarquee large={true} />
-            </div>
-          </div>
-          <EmojiMarquee />
-          <div className="rounded-sm border-[3px] border-black w-11/12 sm:max-w-2xl tabular-nums shadow-container shadow-gray-900/70 overflow-y-auto mb-8 mt-4 lg:mt-0">
-            <div className="py-1 flex flex-row bg-amber-500 border-b-[3px] border-b-black">
-              <p className="font-bold text-center w-5/12 sm:w-1/4">route</p>
-              <p className="flex-1 font-bold text-center pl-2">destination</p>
-              <p className="sm:w-20"></p>
-            </div>
-            <div className="flex flex-col overflow-y-scroll max-h-[30rem]">
-              {data.map((listing: ConfigData) => (
-                <Listing
-                  key={listing.route}
-                  route={listing.route}
-                  destination={listing.destination}
-                  visits={listing.visits}
-                  fallbackData={fallbackData}
-                  fetchedBefore={fetchedBefore}
-                  status={listing.status}
-                />
-              ))}
-            </div>
-            <Add
-              open={addDialogOpen}
-              setOpen={setAddDialogOpen}
-              fallbackData={fallbackData}
-            />
-          </div>
-        </div>
+    <>
+      <div className="flex flex-col overflow-y-scroll max-h-[30rem]">
+        {data.map((listing: ConfigData) => (
+          <Listing
+            key={listing.route}
+            route={listing.route}
+            destination={listing.destination}
+            visits={listing.visits}
+            fallbackData={fallbackData}
+            fetchedBefore={fetchedBefore}
+            status={listing.status}
+          />
+        ))}
       </div>
-    </div>
+      <Add
+        open={addDialogOpen}
+        setOpen={setAddDialogOpen}
+        fallbackData={fallbackData}
+      />
+    </>
   );
 };
 
