36b358a95207d0fa42cf5710b02d7a36a2803a4d
Matthew Stanciu <mattbstanciu@gmail.com>
2023-03-05 17:23:59 -0500

    Indicate when signing in

--- dash/components/sign-in-button.tsx
+++ dash/components/sign-in-button.tsx
@@ -5,7 +5,12 @@ export default function SignOutButton() {
   return (
     <button
       className="rounded-sm px-1 font-bold w-fit border-2 border-gray-600 hover:bg-gray-600 hover:text-gray-100 transition duration-100"
-      onClick={() => signIn("github")}
+      id="sign-in-button"
+      onClick={() => {
+        signIn("github");
+        const button = document.getElementById("sign-in-button");
+        if (button) button.innerHTML = "Signing in...";
+      }}
     >
       Sign In
     </button>
--- dash/components/sign-in-modal/sign-in-button.tsx
+++ dash/components/sign-in-modal/sign-in-button.tsx
@@ -4,8 +4,13 @@ import { signIn } from "next-auth/react";
 export default function SignInButton() {
   return (
     <button
+      id="sign-in-button-modal"
       className="bg-amber-100 text-amber-700 disabled:bg-mauve4 disabled:text-mauve11 hover:bg-amber-200 focus:shadow-black inline-flex h-[35px] items-center justify-center rounded-sm border-2 border-black focus:border-[2.5px] shadow-button shadow-gray-800/80 px-[15px] font-medium leading-none focus:outline-none"
-      onClick={() => signIn("github")}
+      onClick={() => {
+        signIn("github");
+        const button = document.getElementById("sign-in-button-modal");
+        if (button) button.innerHTML = "Signing in...";
+      }}
     >
       Sign In
     </button>
