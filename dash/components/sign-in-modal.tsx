4a2f07e7ef915bf683e7e7ba9d8ad2f21b7fb024
Matthew Stanciu <mattbstanciu@gmail.com>
2023-03-05 15:45:48 -0500

    add backdrop blur

--- dash/components/sign-in-modal.tsx
+++ dash/components/sign-in-modal.tsx
@@ -14,7 +14,7 @@ const Add = ({
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
+        <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 backdrop-blur-sm" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-sm bg-white border-[3px] border-black p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-mauve12 m-0 text-[17px] font-bold">
             Sign In
