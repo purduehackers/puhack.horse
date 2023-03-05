943a09e01930722a5dbfb9d0e88e2d1ef8e6f8d6
Matthew Stanciu <mattbstanciu@gmail.com>
2023-03-05 14:43:34 -0500

    Reorganize page

--- dash/app/page.tsx
+++ dash/app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
+import EmojiMarquee from "../components/emoji-marquee";
 import RouteList from "../components/route-list";
+import UserInfo from "../components/user/user-info";
 import { server } from "../lib/helpers";
 import { getCurrentUser } from "../lib/session";
 
@@ -11,5 +13,38 @@ export default async function Index() {
     },
   }).then((r) => r.json());
 
-  return <RouteList fallbackData={data} user={user} />;
+  return (
+    <div className="flex flex-col">
+      <UserInfo user={user} />
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center lg:items-start justify-center lg:flex-row w-full lg:w-11/12 xl:w-full gap-x-8">
+          <div className="flex flex-col gap-4 mx-4 max-w-sm">
+            <h1 className="text-center text-amber-600 font-mono text-3xl sm:text-4xl font-bold">
+              <span className="select-none inline-block shake">🐴</span>{" "}
+              puhack.horse{" "}
+              <span className="select-none inline-block shake">🐴</span>
+            </h1>
+            <hr className="h-1 border-0 bg-gray-600 rounded-sm" />
+            <div className="flex flex-col gap-2">
+              <p>
+                this is the dashboard for purdue hackers' link shortener.
+                welcome!!!
+              </p>
+              <p>
+                to edit something, just click on it in the table. your changes
+                will reflect on the dashboard immediately and update in edge
+                config in the background. it will take a few seconds to
+                propagate, but shouldn't take longer than that.
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <EmojiMarquee large={true} />
+            </div>
+          </div>
+          <EmojiMarquee />
+          <RouteList fallbackData={data} user={user} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- dash/components/route-list.tsx
+++ dash/components/route-list.tsx
@@ -5,9 +5,7 @@ import useSWR from "swr";
 import { fetcher, server } from "../lib/helpers";
 import { ConfigData, User } from "../types/types";
 import Add from "./add";
-import EmojiMarquee from "./emoji-marquee";
 import Listing from "./listing";
-import UserInfo from "./user/user-info";
 
 const RouteList = ({
   fallbackData,
@@ -40,69 +38,38 @@ const RouteList = ({
   });
 
   return (
-    <div className="flex flex-col">
-      <UserInfo user={user} />
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col items-center lg:items-start justify-center lg:flex-row w-full lg:w-11/12 xl:w-full gap-x-8">
-          <div className="flex flex-col gap-4 mx-4 max-w-sm">
-            <h1 className="text-center text-amber-600 font-mono text-3xl sm:text-4xl font-bold">
-              <span className="select-none inline-block shake">🐴</span>{" "}
-              puhack.horse{" "}
-              <span className="select-none inline-block shake">🐴</span>
-            </h1>
-            <hr className="h-1 border-0 bg-gray-600 rounded-sm" />
-            <div className="flex flex-col gap-2">
-              <p>
-                this is the dashboard for purdue hackers' link shortener.
-                welcome!!!
-              </p>
-              <p>
-                to edit something, just click on it in the table. your changes
-                will reflect on the dashboard immediately and update in edge
-                config in the background. it will take a few seconds to
-                propagate, but shouldn't take longer than that.
-              </p>
-            </div>
-            <div className="flex justify-center">
-              <EmojiMarquee large={true} />
-            </div>
-          </div>
-          <EmojiMarquee />
-          <div className="rounded-sm border-[3px] border-black w-11/12 sm:max-w-2xl tabular-nums shadow-container shadow-gray-900/70 overflow-y-auto mb-8 mt-4 lg:mt-0">
-            <div className="py-1 flex flex-row bg-amber-500 border-b-[3px] border-b-black">
-              <p className="font-bold text-center w-5/12 sm:w-1/4">route</p>
-              <p className="flex-1 font-bold text-center pl-2">destination</p>
-              <p className="sm:w-20"></p>
-            </div>
-            <div className="flex flex-col overflow-y-scroll max-h-[30rem]">
-              {data.map((listing: ConfigData) => (
-                <Listing
-                  user={user}
-                  key={listing.route}
-                  route={listing.route}
-                  destination={listing.destination}
-                  visits={listing.visits}
-                  fallbackData={fallbackData}
-                  fetchedBefore={fetchedBefore}
-                  status={listing.status}
-                />
-              ))}
-            </div>
-            {user ? (
-              <Add
-                open={addDialogOpen}
-                setOpen={setAddDialogOpen}
-                fallbackData={fallbackData}
-              />
-            ) : (
-              <div className="py-2 pl-2 bg-amber-500 font-bold flex flex-row items-center gap-1 hover:bg-amber-400 transition duration-100 cursor-pointer sticky w-full bottom-0 border-t-[3px] border-black">
-                <Plus />
-                <p>add</p>
-              </div>
-            )}
-          </div>
-        </div>
+    <div className="rounded-sm border-[3px] border-black w-11/12 sm:max-w-2xl tabular-nums shadow-container shadow-gray-900/70 overflow-y-auto mb-8 mt-4 lg:mt-0">
+      <div className="py-1 flex flex-row bg-amber-500 border-b-[3px] border-b-black">
+        <p className="font-bold text-center w-5/12 sm:w-1/4">route</p>
+        <p className="flex-1 font-bold text-center pl-2">destination</p>
+        <p className="sm:w-20"></p>
       </div>
+      <div className="flex flex-col overflow-y-scroll max-h-[30rem]">
+        {data.map((listing: ConfigData) => (
+          <Listing
+            user={user}
+            key={listing.route}
+            route={listing.route}
+            destination={listing.destination}
+            visits={listing.visits}
+            fallbackData={fallbackData}
+            fetchedBefore={fetchedBefore}
+            status={listing.status}
+          />
+        ))}
+      </div>
+      {user ? (
+        <Add
+          open={addDialogOpen}
+          setOpen={setAddDialogOpen}
+          fallbackData={fallbackData}
+        />
+      ) : (
+        <div className="py-2 pl-2 bg-amber-500 font-bold flex flex-row items-center gap-1 hover:bg-amber-400 transition duration-100 cursor-pointer sticky w-full bottom-0 border-t-[3px] border-black">
+          <Plus />
+          <p>add</p>
+        </div>
+      )}
     </div>
   );
 };
