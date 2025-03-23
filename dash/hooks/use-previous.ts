7d986f9c32adedb4645e71547260313345fdd311
Eric Park <me@ericswpark.com>
2025-03-23 15:45:33 -0400

    Update to NextJS 15

--- dash/hooks/use-previous.ts
+++ dash/hooks/use-previous.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
 export default function usePrevious<T>(value: T): T {
-  const ref: any = useRef<T>();
+  const ref: any = useRef<T>(undefined);
   useEffect(() => {
     ref.current = value;
   }, [value]);
